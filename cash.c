ee30d46ec62612c329a895803849d181fb64a6fe
Gabriel Marchetti <64610841+subnaps@users.noreply.github.com>
2021-05-13 01:00:35 -0300

    Create cash.c

---
+++ cash.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    float x;
+    do
+    {
+        x = get_float("Change owed:");
+    }
+    while(x<0);
+    int c=round(x*100);
+    int v=round((c-c%25)/25);
+    int d=round((c-v*25-c%10)/10);
+    int ci=round((c-v*25-d*10-c%5)/5);
+    int um=round(c-v*25-d*10-ci*5);
+    printf("%i\n",(v+d+ci+um));
+}
